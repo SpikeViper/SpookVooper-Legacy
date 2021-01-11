717e0c74931ca7c366f12aa0aff626802d4b0218
BuildTools <kobe.albright@gmail.com>
2021-01-11 14:53:47 -0500

    Fix raxor issue

--- SpookVooper/Startup.cs
+++ SpookVooper/Startup.cs
@@ -211,7 +211,6 @@ namespace SpookVooper
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
