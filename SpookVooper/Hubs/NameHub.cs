6e0214f8abcd4cfcfd68d2e34d9a9bde3e6d63a9
Coca <coca16622@gmail.com>
2021-11-18 20:58:00 +0000

    Name Signalr

---
+++ SpookVooper/Hubs/NameHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using SpookVooper.Common.Managers;
+
+namespace SpookVooper.Web.Hubs
+{
+    public class NameHub : Hub
+    {
+        public static IHubContext<NameHub> Current;
+    }
+}
